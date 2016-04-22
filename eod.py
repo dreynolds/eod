3add6afe6bb2390363348f2f8d74aa39f4144c4d
David Reynolds <david@reynoldsfamily.org.uk>
2016-04-22 10:29:31 +0100

    Add file

---
+++ eod.py
@@ -0,0 +1,16 @@
+import datetime
+import math
+
+def main():
+    """Calculate the minutes until the end of the day"""
+    # Get the difference between now and the end of the day
+    now = datetime.datetime.now()
+    eod = datetime.datetime.now().replace(hour=17, minute=30, second=00)
+    diff = eod - now
+    # Turn it into minutes, including fractions
+    minutes = float(diff.seconds) / 60.0
+    # Round up and return without fractions
+    print "%d" % math.ceil(minutes)
+
+if __name__ == '__main__':
+    main()
